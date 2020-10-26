4dcf48a4944470d1c6cf82c5dd71f7e1d26f8aa4
Dmitrii Alexandrov <d.a.a.89@mail.ru>
2020-10-26 16:48:00 +0400

    update billing data form validation
    update billing data form validation

--- src/components/BillingDataForm/BillingDataForm.tsx
+++ src/components/BillingDataForm/BillingDataForm.tsx
@@ -1,20 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { Form } from 'semantic-ui-react';
 import { FormHelper } from '../../helpers/form.helper';
-import { TValidationFunction, ValidationHelper } from '../../helpers/validation.helper';
+import { ValidationHelper } from '../../helpers/validation.helper';
 
 import { BillingForm } from '../../models/billing-form.model';
+import { useFormValidation } from '../FormValidationProvider/FormValidationProvider';
 
 import './BillingDataForm.css';
 
 interface IBillingDataFormProps {
     billingForm: BillingForm;
+    showErrors: boolean;
     onChange: (billing: BillingForm) => void;
+    onValidChange: (isValid: boolean) => void;
 }
 
 function BillingDataForm(props: IBillingDataFormProps) {
-    const [errors, setErrors] = useState({} as {[key: string]: any});
-    const [isEUCountrySelected, setIsEUCountrySelected] = useState(false);
+    const [isEUCountrySelected, setEUCountrySelected] = useState(false);
+    const [isStateDisabled, setStateDisabled] = useState(false);
+    const { validate, hasErrors, clearFieldError, showAllErrors, errors } = useFormValidation();
 
     function onChange(propName: string, value: string | null): void {
         props.onChange({
@@ -22,53 +26,63 @@ function BillingDataForm(props: IBillingDataFormProps) {
             billing: {
                 ...props.billingForm.billing,
                 [propName]: value,
-            }
+            },
         } as BillingForm);
-    }
-
-    function validate(fieldName: string, value: string, validators: TValidationFunction[]) {
-        setErrors(prev => {
-            return {
-                ...prev,
-                [fieldName]: ValidationHelper.validate(value, validators),
-            };
-        });
+        clearFieldError(propName);
     }
 
     const { billingForm } = props;
     const { billing } = billingForm;
 
+    //#region Validation rules
+
     useEffect(() => {
         validate('name', billing.name, [ValidationHelper.requiredValidator, ValidationHelper.nameValidator]);
-    }, [billing.name]);
+    }, [billing.name, validate]);
 
     useEffect(() => {
         validate('company', billing.company, [ValidationHelper.requiredValidator, ValidationHelper.minLengthValidator(3)]);
-    }, [billing.company]);
+    }, [billing.company, validate]);
 
     useEffect(() => {
-        validate('city', billing.city, [ValidationHelper.requiredValidator]);
-    }, [billing.city]);
+        validate('country', billing.country, [ValidationHelper.requiredValidator]);
+    }, [billing.country, validate]);
 
     useEffect(() => {
-        validate('vatId', billing.vatId, [ValidationHelper.requiredValidator, ValidationHelper.vatValidator]);
-    }, [billing.country, billing.vatId]);
+        const states = billingForm.statesMap[billing.country];
+        const validators = states && states.length ? [ValidationHelper.requiredValidator] : [];
+        setStateDisabled(!states.length)
+        validate('state', billing.state, validators);
+    }, [billing.country, billing.state, billingForm.statesMap, validate]);
 
     useEffect(() => {
-        validate('state', billing.state, [ValidationHelper.requiredValidator]);
-    }, [billing.state]);
+        const validators = isEUCountrySelected ? [ValidationHelper.requiredValidator, ValidationHelper.vatValidator] : [];
+        validate('vatId', billing.vatId, validators);
+    }, [billing.vatId, isEUCountrySelected, validate]);
 
     useEffect(() => {
         validate('zipCode', billing.zipCode, [ValidationHelper.requiredValidator, ValidationHelper.minLengthValidator(3)]);
-    }, [billing.zipCode]);
+    }, [billing.zipCode, validate]);
 
     useEffect(() => {
         validate('address', billing.address, [ValidationHelper.requiredValidator, ValidationHelper.minLengthValidator(7)]);
-    }, [billing.address]);
+    }, [billing.address, validate]);
+
+    //#endregion
 
     useEffect(() => { 
-        setIsEUCountrySelected(!ValidationHelper.validate(billing.country, [ValidationHelper.euCountryValidator]));
+        setEUCountrySelected(!ValidationHelper.validate(billing.country, [ValidationHelper.euCountryValidator]));
      }, [billing.country]);
+
+    useEffect(() => { 
+        props.onValidChange(!hasErrors());
+    }, [ hasErrors, props]);
+
+    useEffect(() => {
+        if (props.showErrors) {
+            showAllErrors();
+        }
+    }, [props.showErrors, showAllErrors]);
 
     if (!billing) {
         return null;
@@ -81,7 +95,7 @@ function BillingDataForm(props: IBillingDataFormProps) {
                     label='Name'
                     value={billing.name}
                     placeholder='e.g. John Smith'
-                    error={errors.name ? { content: errors.name } : null}
+                    error={errors.name && errors.name.isVisible ? { content: errors.name.message } : null}
                     onChange={FormHelper.getOnInputChangeHandler('name', onChange)} />
             </Form.Field>
             <Form.Field>
@@ -89,7 +103,7 @@ function BillingDataForm(props: IBillingDataFormProps) {
                     label='Company'
                     value={billing.company}
                     placeholder='e.g. AppFollow'
-                    error={errors.company ? { content: errors.company } : null}
+                    error={errors.company && errors.company.isVisible ? { content: errors.company.message } : null}
                     onChange={FormHelper.getOnInputChangeHandler('company', onChange)} />
             </Form.Field>
             <Form.Group widths='equal'>
@@ -98,14 +112,15 @@ function BillingDataForm(props: IBillingDataFormProps) {
                     value={billing.country}
                     options={billingForm.countries}
                     placeholder='Select country'
-                    error={errors.country ? { content: errors.country } : null}
+                    error={errors.country && errors.country.isVisible ? { content: errors.country.message } : null}
                     onChange={FormHelper.getOnSelectChangeHandler('country', onChange)} />
                 <Form.Select
                     label='State'
                     value={billing.state}
                     options={billingForm.statesMap[billing.country]}
                     placeholder='Select state'
-                    error={errors.state ? { content: errors.state } : null}
+                    disabled={isStateDisabled}
+                    error={errors.state && errors.state.isVisible ? { content: errors.state.message } : null}
                     onChange={FormHelper.getOnSelectChangeHandler('state', onChange)} />
             </Form.Group>
             <Form.Group widths='equal'>
@@ -113,14 +128,14 @@ function BillingDataForm(props: IBillingDataFormProps) {
                     label='City'
                     value={billing.city}
                     placeholder='e.g. Helsinki'
-                    error={errors.city ? { content: errors.city } : null}
+                    error={errors.city && errors.city.isVisible ? { content: errors.city.message } : null}
                     onChange={FormHelper.getOnInputChangeHandler('city', onChange)} />
                 { isEUCountrySelected && (
                 <Form.Input
                     label='VAT ID'
                     value={billing.vatId}
                     placeholder='e.g. FI99999999'
-                    error={errors.vatId ? { content: errors.vatId } : null}
+                    error={errors.vatId && errors.vatId.isVisible ? { content: errors.vatId.message } : null}
                     onChange={FormHelper.getOnInputChangeHandler('vatId', onChange)} />
                 )}
             </Form.Group>
@@ -129,7 +144,7 @@ function BillingDataForm(props: IBillingDataFormProps) {
                     label='ZIP Code'
                     value={billing.zipCode}
                     placeholder='e.g. 410012'
-                    error={errors.zipCode ? { content: errors.zipCode } : null}
+                    error={errors.zipCode && errors.zipCode.isVisible ? { content: errors.zipCode.message } : null}
                     onChange={FormHelper.getOnInputChangeHandler('zipCode', onChange)} />
             </Form.Field>
             <Form.Field>
@@ -138,10 +153,10 @@ function BillingDataForm(props: IBillingDataFormProps) {
                     value={billing.address}
                     rows='3'
                     placeholder='e.g. 2540 Iroquois Ave.'
-                    error={errors.address ? { content: errors.address } : null}
+                    error={errors.address && errors.address.isVisible ? { content: errors.address.message } : null}
                     onChange={FormHelper.getOnTextAreaChangeHandler('address', onChange)} />
             </Form.Field>
-        </Form>
+    </Form>
     );
 }
 
